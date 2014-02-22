a3eba4155a4f385588afb4e1a81822ff7e142e9b
Andre Levi <andrelevi09@gmail.com>
2014-02-22 14:28:00 -0800

    Added basic server routing.

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static('/web/sites/sea.bike')
-).listen(80);
+var express = require('express'),
+    http = require('http'),
+    ElastiSearchClient = require('elasticsearchclient');
 
+
+var port = 3000;
+
+var app = express();
+app.use(express.bodyParser()); // Look up warning
+
+
+app.post('/api/reports', function(req, res) {
+
+});
+
+app.get('/add-report', function(req, res) {
+    res.sendfile('report-form.html');
+});
+
+app.get('*', function(req, res) {
+    res.sendfile('maps.html');
+});
+
+http.createServer(app).listen(port, function () {
+    console.log('Express server listening on port: ' + port);
+});
